ae816aae7751e65ea3346db51a6f2777c7e86e7c
MetricManifold <ssilber@uwo.ca>
2023-09-20 00:02:39 -0400

    fixed namespace resolution for Time

--- examples/simultaneous-configs/main.cpp
+++ examples/simultaneous-configs/main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[])
 {
 #if (defined(MODEL_INCLUDE_HEADER) || !defined(USING_MODEL_SELECTION))
 
-	Time t("entire simulation");
+    symphas::Time t("entire simulation");
 	symphas::init(argv[1], argv + 2, argc - 2);
 
 #	ifdef USING_CONF
