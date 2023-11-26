12e792dfe7c89bb7db101d5dd167d1db9dbe20b6
MetricManifold <ssilber@uwo.ca>
2023-11-26 09:22:59 -0500

    uploading my first simulation example

---
+++ myfirstsim/driver.cpp
@@ -0,0 +1,47 @@
+#include "symphas.h"
+#include "../examples/solvers/solverft.h"
+
+// model definition: note that power is NOT denoted using ^
+MODEL(A, (SCALAR), EVOLUTION(dop(1) = lap(op(1)) - (power(op(1), 2) - 1_n) * op(1)))
+
+INITIAL_CONDITION_EQUATION(INIT, (2, 3), sin(x) + cos(y))
+
+int main()
+{
+        
+    auto init = [](auto index, const auto* dims, auto dimension) 
+    {
+        int pos[2]{};
+        grid::get_grid_position(pos, dims, index);
+        return std::sin(pos[0]) * std::cos(pos[1]);    
+    };
+
+    symphas::interval_element_type interval(1000);
+    symphas::b_data_type bdata(2, BoundaryType::PERIODIC);
+	symphas::init_data_type tdata(Inside::UNIFORM, { -1, 1 });
+
+    symphas::problem_parameters_type parameters(1);
+
+    using namespace symphas;
+
+    auto tdata1 = Inside::UNIFORM <<= { -1, 1 };
+    auto tdat02 = Inside::CIRCLE / InsideTag::RANDOM <<= { -1, 1 };
+    auto tdata2 = Inside::EXPRESSION << "INIT";
+    auto tdat3a = Inside::CIRCLE / InsideTag::VARA <<= {1, 1};
+    auto tdat4a = Inside::LAMBDA << init;
+    symphas::init_data_functor lambd(init);
+    symphas::init_data_type tdata00(lambd);
+
+    //another way to make an interval:
+    auto interval0 = BoundaryType::PERIODIC || 100_h / 0.1_dh || BoundaryType::PERIODIC;
+    auto grid_intervals = interval0 * interval0;
+
+    // set the parameters using 'problem spec notation'
+    parameters[0] << interval*interval << bdata << tdata;
+
+    
+
+    // the model typename is derived from the model definition, the given name is inserted into 'model_*_t'. 
+    model_A_t<2, SolverFT<Stencil2d2h<>>> model{ parameters };
+    symphas::find_solution(model, 0.1, 50);
+}
