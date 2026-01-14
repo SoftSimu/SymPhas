74ade93a8a71dcf42c17f8aa635a8ca31ad418da
Liam Wilson <lwils66@uwo.ca>
2026-01-13 19:39:26 -0500

    Added a model with a triangle wave external field

--- examples/models/modelinclude.h
+++ examples/models/modelinclude.h
@@ -71,6 +71,27 @@ MODEL(MagneticPFC2013, (SCALAR, VECTOR),
 )
 LINK_WITH_NAME(MagneticPFC2013, MAGNETICPFC2013)
 
+MODEL(FMPFCLinearField, (SCALAR, VECTOR),
+      PROVISIONAL_DEF((SCALAR, VECTOR, SCALAR), 
+        var(1) <= PoissonSolver(curl(op(2))),
+        var(2) <= grady(var(1)) * e(x) - gradx(var(1)) * e(y),
+        var(3) <= c(11) * asin(sin(2 * pi_n * t / c(12)))
+      )
+      EVOLUTION(
+            dop(1) = lap(c(1) * op(1) + c(2) * op(1) +
+                        c(2) * 2_n * lap(op(1)) + c(2) * bilap(op(1)) -
+                        c(3) * power(op(1), 2) + c(4) * power(op(1), 3) -
+                        c(8) * op(1) * dot(op(2), op(2))) +
+                  lap(c(10) * dot(op(2), grad(op(1))) * div(op(2))) +
+                  lap(c(10) * dot(op(2), grad(dot(grad(op(1)), op(2))))),
+            dop(2) = c(6) * c(6) * lap(op(2)) - c(7) * op(2) +
+                  c(8) * power(op(1), 2) * op(2) -
+                  c(9) * op(2) * dot(op(2), op(2)) +
+                  c(10) * grad(op(1)) * dot(op(2), grad(op(1))) + var(2) + var(3) * e(y)
+      )
+)
+LINK_WITH_NAME(FMPFCLinearField, FMPFCLINEARFIELD)
+
 // #include "advancedmodeldefs.h"
 // #include "modeldefinitions.h"
 // #include "pfcdefs.h"
