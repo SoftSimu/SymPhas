4838591f430d145670e30b9c8963f90dc6c1bdf7
Steven Silber <stevensilber@microsoft.com>
2026-04-12 20:25:26 -0400

    update poisson model defs

--- examples/models/modelinclude.h
+++ examples/models/modelinclude.h
@@ -54,18 +54,21 @@ LINK_WITH_NAME(CONV, CONVOLUTION)
 MODEL(MagneticPFC2013, (SCALAR, VECTOR),
       PROVISIONAL_DEF((SCALAR, VECTOR), 
         var(1) <= PoissonSolver(curl(op(2))),
-        var(2) <= var(1) * e(0))
-          EVOLUTION(
-              dop(1) = lap(c(1) * op(1) + c(2) * op(1) +
-                           c(2) * 2_n * lap(op(1)) + c(2) * bilap(op(1)) -
-                           c(3) * power(op(1), 2) + c(4) * power(op(1), 3) -
-                           c(8) * op(1) * dot(op(2), op(2))) +
-                       lap(c(10) * dot(op(2), grad(op(1))) * div(op(2))) +
-                       lap(c(10) * dot(op(2), grad(dot(grad(op(1)), op(2))))),
-              dop(2) = c(6) * c(6) * lap(op(2)) - c(7) * op(2) +
-                       c(8) * power(op(1), 2) * op(2) -
-                       c(9) * op(2) * dot(op(2), op(2)) +
-                       c(10) * grad(op(1)) * dot(op(2), grad(op(1))) + var(2)))
+        var(2) <= grady(var(1)) * e(x) - gradx(var(1)) * e(y)
+      )
+      EVOLUTION(
+            dop(1) = lap(c(1) * op(1) + c(2) * op(1) +
+                        c(2) * 2_n * lap(op(1)) + c(2) * bilap(op(1)) -
+                        c(3) * power(op(1), 2) + c(4) * power(op(1), 3) -
+                        c(8) * op(1) * dot(op(2), op(2))) +
+                  lap(c(10) * dot(op(2), grad(op(1))) * div(op(2))) +
+                  lap(c(10) * dot(op(2), grad(dot(grad(op(1)), op(2))))),
+            dop(2) = c(6) * c(6) * lap(op(2)) - c(7) * op(2) +
+                  c(8) * power(op(1), 2) * op(2) -
+                  c(9) * op(2) * dot(op(2), op(2)) +
+                  c(10) * grad(op(1)) * dot(op(2), grad(op(1))) + var(2)
+      )
+)
 LINK_WITH_NAME(MagneticPFC2013, MAGNETICPFC2013)
 
 // #include "advancedmodeldefs.h"
